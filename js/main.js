cda04680973f30c8276630f16d74439e8642e651
rinox3005 <peppe.rinox@gmail.com>
2024-05-19 21:39:24 +0200

    [UPD] added switch for all cases, merged it in a functions with element creation and added event listeners to swap icons from select

--- js/main.js
+++ js/main.js
@@ -120,16 +120,75 @@ console.log(icons);
 const filteredIcons = document.getElementById("filtered-icons");
 const filter = document.getElementById("filter");
 
-icons.forEach((element) => {
-    const div = document.createElement("div");
-    const i = document.createElement("i");
-    const h3 = document.createElement("h3");
-    div.classList.add("card");
-    i.classList.add(`${element.family}`);
-    i.classList.add(`${element.prefix}${element.name}`);
-    i.style.color = element.color;
-    h3.innerText = element.name;
-    div.appendChild(i);
-    div.appendChild(h3);
-    filteredIcons.appendChild(div);
+
+
+
+function swapIcons(filter) {
+
+    icons.forEach((element) => {
+
+        const div = document.createElement("div");
+        const i = document.createElement("i");
+        const h3 = document.createElement("h3");
+
+        switch (filter) {
+
+            case "all":
+                div.classList.add("card");
+                i.classList.add(`${element.family}`);
+                i.classList.add(`${element.prefix}${element.name}`);
+                i.style.color = element.color;
+                h3.innerText = element.name;
+                div.appendChild(i);
+                div.appendChild(h3);
+                filteredIcons.appendChild(div);
+
+            case "animal":
+                if (element.type === filter) {
+                    div.classList.add("card");
+                    i.classList.add(`${element.family}`);
+                    i.classList.add(`${element.prefix}${element.name}`);
+                    i.style.color = element.color;
+                    h3.innerText = element.name;
+                    div.appendChild(i);
+                    div.appendChild(h3);
+                    filteredIcons.appendChild(div);
+                }
+
+            case "vegetable":
+                if (element.type === filter) {
+                    div.classList.add("card");
+                    i.classList.add(`${element.family}`);
+                    i.classList.add(`${element.prefix}${element.name}`);
+                    i.style.color = element.color;
+                    h3.innerText = element.name;
+                    div.appendChild(i);
+                    div.appendChild(h3);
+                    filteredIcons.appendChild(div);
+                }
+
+            case "user":
+                if (element.type === filter) {
+                    div.classList.add("card");
+                    i.classList.add(`${element.family}`);
+                    i.classList.add(`${element.prefix}${element.name}`);
+                    i.style.color = element.color;
+                    h3.innerText = element.name;
+                    div.appendChild(i);
+                    div.appendChild(h3);
+                    filteredIcons.appendChild(div);
+                }
+        }
+    });
+}
+
+filter.addEventListener("change", () => {
+    const filterValue = filter.value;
+    swapIcons(filterValue);
 });
+
+window.addEventListener("load", () => {
+    const filterValue = filter.value;
+    swapIcons(filterValue);
+});
+
